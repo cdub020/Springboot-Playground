317331ee488658408b72c773b38d8891d059e7ee
cdub020 <63630268+cdub020@users.noreply.github.com>
2020-07-18 09:08:07 -0700

    Spring Math

--- src/main/java/com/galvanize/HelloController.java
+++ src/main/java/com/galvanize/HelloController.java
@@ -2,13 +2,22 @@ package com.galvanize.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello from Spring!";
+    @GetMapping("math/pi")
+    public double helloWorld() {
+        return Math.PI;
+    }
+
+    @PostMapping("/thispost")
+    public String respond() {
+        return "You just posted!";
     }
 
 }
