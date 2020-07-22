a94c412956f7ed51a758cb39d4bacaa5de132b4e
cdub020 <63630268+cdub020@users.noreply.github.com>
2020-07-22 15:54:01 -0700

    CRUD enpoints complete

--- src/main/java/com/galvanize/demo/LessonsController.java
+++ src/main/java/com/galvanize/demo/LessonsController.java
@@ -2,6 +2,8 @@ package com.galvanize.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController{
@@ -19,5 +21,15 @@ public class LessonsController{
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Lesson> byid(@PathVariable long id){
+        return this.repository.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletebyid(@PathVariable long id){
+        this.repository.deleteById(id);
     }
 }
